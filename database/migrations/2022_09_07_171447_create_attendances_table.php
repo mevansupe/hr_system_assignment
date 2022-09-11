c767409525a376c98b8f7fbd43a0af26687e39ac
Mevan Supeshala <54064198+mevansupe@users.noreply.github.com>
2022-09-11 19:54:47 +0530

    migrations completed

---
+++ database/migrations/2022_09_07_171447_create_attendances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('checkin');
+            $table->dateTime('checkout');
+            $table->decimal('total_working_hours');
+            $table->foreignId('employee_id')->index();
+            $table->foreignId('schedule_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
---
+++ database/migrations/2022_09_07_172032_create_attendance_faults_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceFaultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_faults', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->foreignId('attendance_id')->index();
+            $table->foreignId('employee_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_faults');
+    }
+}
